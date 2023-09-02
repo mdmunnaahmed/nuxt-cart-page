379cc318aacf47d91660f4b59583478cb33f54ff
mdmunnaahmed <msdmunna77@gmail.com>
2023-09-02 19:53:14 +0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>This is Home page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
